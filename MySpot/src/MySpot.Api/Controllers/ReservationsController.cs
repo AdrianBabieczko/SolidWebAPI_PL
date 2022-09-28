6a3fd74cc31c569d7a706def41a16df9f152485c
AdrianBabieczko <adrian.babieczko@gmail.com>
2022-09-28 18:31:07 +0200

    Add rest client, remove default controllers, add new controler

---
+++ MySpot/src/MySpot.Api/Controllers/ReservationsController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MySpot.Api.Controllers;
+
+
+public class ReservationsController : ControllerBase
+{
+    [HttpGet(template:"get")]
+    public void Get()
+    {
+        
+    }
+
+    [HttpGet(template:"get2")]
+    public void Get2()
+    {
+        
+    }
+}
