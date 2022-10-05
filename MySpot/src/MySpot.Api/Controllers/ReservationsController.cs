625c9d157867885eeeeba7b2379d1a8f9ebd0994
AdrianBabieczko <adrian.babieczko@gmail.com>
2022-10-05 19:24:10 +0200

    Add methods to ReservationsController

--- MySpot/src/MySpot.Api/Controllers/ReservationsController.cs
+++ MySpot/src/MySpot.Api/Controllers/ReservationsController.cs
@@ -7,30 +7,60 @@ namespace MySpot.Api.Controllers;
 [Route("[controller]")]
 public class ReservationsController : ControllerBase
 {
-    private int _id = 1;
-    private readonly List<Reservation> _reservations = new();
+    private static int _id = 1;
+    
+    private static readonly List<Reservation> Reservations = new();
 
-    private readonly List<string> _parkingSpotNames = new()
+    private static readonly List<string> ParkingSpotNames = new()
     {
         "P1", "P2", "P3", "P4", "P5"
     };
-    
+
     [HttpGet]
-    public void Get()
+    public ActionResult<IEnumerable<Reservation>> Get() => Ok(Reservations);
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Reservation> Get(int id)
     {
+        var reservation = Reservations.SingleOrDefault(x => x.Id == id);
+
+        if (reservation is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(reservation);
     }
     
+
     [HttpPost]
-    public void Post(Reservation reservation)
+    public ActionResult Post(Reservation reservation)
     {
-        if (_parkingSpotNames.All(x=>x != reservation.ParkingSpotName))
+        if (ParkingSpotNames.All(x=>x != reservation.ParkingSpotName))
         {
-            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-            return;
+            return BadRequest();
+        }
+
+        reservation.Date = DateTime.UtcNow.AddDays(1).Date;
+        
+        var reservationAlreadyExists = Reservations.Any(x =>
+            x.ParkingSpotName == reservation.ParkingSpotName && x.Date.Date == reservation.Date.Date);
+
+        if (reservationAlreadyExists)
+        {
+            return BadRequest();
         }
         
         reservation.Id = _id;
-        reservation.Date = DateTime.UtcNow.AddDays(1).Date;
         _id++;
+        Reservations.Add(reservation);
+
+        return CreatedAtAction(nameof(Get), new { id = reservation.Id }, null);
+    }
+
+    [HttpPut]
+    public ActionResult Put(Reservation reservation)
+    {
+        return Ok();
     }
 }
